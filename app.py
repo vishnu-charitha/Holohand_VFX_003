8556387d4bc444877f0a2c904d7641e5a52a31d4
vishnu-charitha <nagarurucharitha@gmail.com>
2026-05-27 10:07:14 +0530

    Fix holo realtime camera

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ st.set_page_config(page_title="HoloHand VFX", layout="wide")
 
 st.title("Real-Time Hand Tracking Hologram System")
 
-# Mediapipe setup
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     max_num_hands=1,
@@ -15,18 +14,16 @@ hands = mp_hands.Hands(
     min_tracking_confidence=0.5
 )
 
-# Open webcam
-cap = cv2.VideoCapture(0)
+camera = st.camera_input("Open Camera")
 
-frame_placeholder = st.empty()
+if camera:
 
-while cap.isOpened():
+    file_bytes = np.asarray(
+        bytearray(camera.read()),
+        dtype=np.uint8
+    )
 
-    success, frame = cap.read()
-
-    if not success:
-        st.error("Camera not working")
-        break
+    frame = cv2.imdecode(file_bytes, 1)
 
     frame = cv2.flip(frame, 1)
 
@@ -43,18 +40,32 @@ while cap.isOpened():
             x = int(hand_landmarks.landmark[9].x * w)
             y = int(hand_landmarks.landmark[9].y * h)
 
-            # HOLO RINGS
-            cv2.circle(frame, (x, y), 80, (255,255,0), 3)
-            cv2.circle(frame, (x, y), 50, (255,255,0), 2)
+            # glowing holo rings
+            overlay = frame.copy()
 
-            # glow dots
+            cv2.circle(overlay, (x, y), 90, (255, 255, 0), 5)
+            cv2.circle(overlay, (x, y), 60, (255, 255, 0), 3)
+
+            # dots around ring
             for angle in range(0, 360, 20):
-                px = int(x + 60 * np.cos(np.radians(angle)))
-                py = int(y + 60 * np.sin(np.radians(angle)))
-                cv2.circle(frame, (px, py), 6, (255,255,0), -1)
+                px = int(x + 75 * np.cos(np.radians(angle)))
+                py = int(y + 75 * np.sin(np.radians(angle)))
+                cv2.circle(
+                    overlay,
+                    (px, py),
+                    5,
+                    (255, 255, 0),
+                    -1
+                )
+
+            frame = cv2.addWeighted(
+                overlay,
+                0.7,
+                frame,
+                0.3,
+                0
+            )
 
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    frame_placeholder.image(frame, use_container_width=True)
-
-cap.release()
+    st.image(frame, use_container_width=True)
